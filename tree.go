563ced99234fd42abda4f78d88eb82b0479da8fd
Satoshi Hirade <shirade222@gmail.com>
2019-02-01 23:30:22 +0900

    Added tree.go, comment and etc...

--- tree.go
+++ tree.go
@@ -1,19 +1,3 @@
-# go-tree
-
-This is an assignment to join [golang.tokyo #21](https://golangtokyo.connpass.com/event/113768/) on 2019/01/28.
-
-## Contents
-
-- [Assignment Details](./data/assignment.pdf)
-- [Comment](./data/comment.md)
-
-## tree.go
-
-```bash
-$ go build ./tree.go
-```
-
-```go
 package main
 
 import (
@@ -93,5 +77,3 @@ func main() {
 		}
 	}
 }
-
-```
